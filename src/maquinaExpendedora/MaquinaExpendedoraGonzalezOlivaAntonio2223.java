09b52c0f14e57e8263da5115f983491806935b33
Antonio <Antonio@Antonio-PC>
2023-03-08 13:29:47 +0100

    Proyecto GonzalezOlivaAntonio2223 comentado con JavaDoc

--- src/maquinaExpendedora/MaquinaExpendedoraGonzalezOlivaAntonio2223.java
+++ src/maquinaExpendedora/MaquinaExpendedoraGonzalezOlivaAntonio2223.java
@@ -1,11 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package máquinaexpendedora;
+
+package maquinaExpendedora;
 
 /**
- *
+ * La clase MaquinaExpendedoraGonzalezOlivaAntonio2223 Representa una máquina expendedora 
+ * de snacks que permite la compra de snacks si se tiene suficiente dinero y hay suficientes unidades en la máquina.
  * @author Antonio González Oliva
  */
 
@@ -14,36 +12,55 @@ public class MaquinaExpendedoraGonzalezOlivaAntonio2223 {// Consideramos máquin
     private double precio;//precio actual de un snack
     private double precio_maxGonzalezOlivaAntonio2223;//precio máximo que puede tener un snack
 
-    /*Constructor por defecto*/
+    /**
+     * Constructor por defecto
+     */
     public MaquinaExpendedoraGonzalezOlivaAntonio2223() {
     
     }
     
     
-     /*Constructor por parámetros*/
+     /**
+     * Constructor por parámetros.
+     * @param unidades Número de snacks disponibles en la máquina expendedora.
+     * @param precio Precio actual de un snack.
+     * @param precio_max Precio máximo que puede tener un snack.
+     */
     public MaquinaExpendedoraGonzalezOlivaAntonio2223(int unidades, double precio, double precio_max) {
         this.unidades = unidades;
         this.precio = precio;
         this.precio_maxGonzalezOlivaAntonio2223 = precio_max;
     }
     
-    /*Método que devuelve el número de snack que tiene la máquina*/
+    /**
+     * Método que devuelve el número de snacks que tiene la máquina.
+     * @return Número de snacks disponibles en la máquina expendedora.
+     */
     public int obtenerUnidades() {
         return this.unidades;
     }
 
-    /*Método que devuelve el precio que tiene cada snack*/
+    /**
+     * Método que devuelve el precio que tiene cada snack.
+     * @return Precio actual de un snack.
+     */
     public double obtenerPrecio() {
         return this.getPrecio();
     }
     
-     /*Método que permite modificar el número de snack dsiponbles de la máquina*/
+     /**
+     * Método que permite modificar el número de snacks disponibles en la máquina expendedora.
+     * @param unidades Número de snacks disponibles en la máquina expendedora.
+     */
     public void modificarUnidades(int unidades) {
         this.unidades = unidades;
     }
     
-     /*Método que permite aumentar el precio de venta, suma al precio actual 
-    el aumento que se indica siempre que no se sobrepase el precio máximo de venta*/
+     /**
+     * Método que permite aumentar el precio de venta de los snacks en la máquina expendedora.
+     * @param aumento Aumento del precio de venta de los snacks en la máquina expendedora.
+     * @throws Exception Si el aumento es menor o igual a 0 o si el precio máximo de venta se sobrepasa.
+     */
     public void aumentarPrecio(double aumento) throws Exception {
         if (aumento <= 0) {
             throw new Exception("El aumento debe ser positivo");
@@ -56,8 +73,14 @@ public class MaquinaExpendedoraGonzalezOlivaAntonio2223 {// Consideramos máquin
     
        
     
-    /*Método que permite obtener un snack si se tiene suficiente dinero y hay 
-suficientes unidades en la máquina*/
+     /**
+     * Método que permite obtener snacks de la máquina expendedora si se tiene suficiente dinero y hay suficientes unidades disponibles.
+     * @param unidades Número de snacks a obtener de la máquina expendedora.
+     * @param dinero Cantidad de dinero a introducir para la compra de los snacks.
+     * @param regaloGonzalezOlivaAntonio2223 Nombre del regalo que se incluirá con la compra de los snacks.
+     * @throws Exception Si el dinero es menor o igual a 0, si no hay suficiente dinero para la compra de los snacks, si el número de snacks a obtener
+     * es mayor que el de las unidades disponibles.
+     */
     public void sacarsnack(int unidades, double dinero, String regaloGonzalezOlivaAntonio2223) throws Exception {
         if (dinero <= 0) {
             throw new Exception("No puede introducirse un valor negativo para el dinero");
@@ -75,28 +98,32 @@ suficientes unidades en la máquina*/
     }
 
     /**
-     * @return the precio
+     * Método que devuelve el precio del snack
+     * @return el precio del Snack
      */
     public double getPrecio() {
         return precio;
     }
 
     /**
-     * @param precio the precio to set
+     * Método que establece el preciod el snack
+     * @param precio establece el precio del snack
      */
     public void setPrecio(double precio) {
         this.precio = precio;
     }
 
     /**
-     * @return the precio_maxGonzalezOlivaAntonio2223
+     * Método que devuelve el precio máximo del sanck
+     * @return el precio máximo del snack
      */
     public double getPrecio_maxGonzalezOlivaAntonio2223() {
         return precio_maxGonzalezOlivaAntonio2223;
     }
 
     /**
-     * @param precio_maxGonzalezOlivaAntonio2223 the precio_maxGonzalezOlivaAntonio2223 to set
+     * Método que establece el precio máximo del snack
+     * @param precio_maxGonzalezOlivaAntonio2223 establece el precio máximo del snack
      */
     public void setPrecio_maxGonzalezOlivaAntonio2223(double precio_maxGonzalezOlivaAntonio2223) {
         this.precio_maxGonzalezOlivaAntonio2223 = precio_maxGonzalezOlivaAntonio2223;
